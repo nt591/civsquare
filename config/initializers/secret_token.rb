de66d60bbab23ceb09779007bf4dfe9c2a8a76d7
Nikhil <jkdman63@gmail.com>
2012-12-13 14:30:06 -0500

    initial commit with README.md

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Civsquare::Application.config.secret_token = 'babddda1d6bf9d507399237a60d7b80dad4492868c4c9c20062f81dcb1b4a352545a2f9361ca54607821ce573cc6ebecaf0b094788995bfb2ef0ed0108ad96ec'
